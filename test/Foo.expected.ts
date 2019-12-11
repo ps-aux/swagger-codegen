a02cd85ca0f11864833696f4637c334d11902aee
psaux <seriouz.accz@gmail.com>
2019-12-11 18:35:04 +0100

    Fix e2e test again

--- test/Foo.expected.ts
+++ test/Foo.expected.ts
@@ -1,209 +1,209 @@
 export const Foo = {
-    entityName: 'Foo',
-    attr: {
-        barId: {
-            type: {
-                name: 'ref',
-                type: {
-                    name: 'Bar'
-                }
-            },
-            name: 'barId',
-            id: 'Foo.barId',
-            refDataPath: 'bar'
+  entityName: 'Foo',
+  attr: {
+    barId: {
+      type: {
+        name: 'ref',
+        type: {
+          name: 'Bar'
+        }
+      },
+      name: 'barId',
+      id: 'Foo.barId',
+      refDataPath: 'bar'
+    },
+    nonRefDataId: {
+      type: {
+        name: 'ref',
+        type: {
+          name: 'Bar'
+        }
+      },
+      name: 'nonRefDataId',
+      id: 'Foo.nonRefDataId'
+    },
+    barNonRef: {
+      type: {
+        name: 'object',
+        type: {
+          name: 'Bar'
+        }
+      },
+      name: 'barNonRef',
+      id: 'Foo.barNonRef'
+    },
+    baz: {
+      type: {
+        name: 'integer'
+      },
+      name: 'baz',
+      id: 'Foo.baz',
+      required: true
+    },
+    aDate: {
+      type: {
+        name: 'date'
+      },
+      name: 'aDate',
+      id: 'Foo.aDate'
+    },
+    detailOnly: {
+      type: {
+        name: 'string'
+      },
+      name: 'detailOnly',
+      id: 'Foo.detailOnly',
+      detailOnly: true
+    },
+    readOnly: {
+      type: {
+        name: 'string'
+      },
+      name: 'readOnly',
+      id: 'Foo.readOnly',
+      readOnly: true
+    },
+    status: {
+      type: {
+        name: 'enum',
+        values: ['a', 'b', 'c']
+      },
+      name: 'status',
+      id: 'Foo.status'
+    },
+    withValidations: {
+      type: {
+        name: 'string'
+      },
+      name: 'withValidations',
+      id: 'Foo.withValidations',
+      validations: [
+        {
+          type: 'pattern',
+          value: /^abc.*$/
         },
-        nonRefDataId: {
-            type: {
-                name: 'ref',
-                type: {
-                    name: 'Bar'
-                }
-            },
-            name: 'nonRefDataId',
-            id: 'Foo.nonRefDataId'
+        {
+          type: 'length',
+          value: {
+            min: 2,
+            max: 5
+          }
         },
-        barNonRef: {
-            type: {
-                name: 'object',
-                type: {
-                    name: 'Bar'
-                }
-            },
-            name: 'barNonRef',
-            id: 'Foo.barNonRef'
-        },
-        baz: {
-            type: {
-                name: 'integer'
-            },
-            name: 'baz',
-            id: 'Foo.baz',
-            required: true
-        },
-        aDate: {
-            type: {
-                name: 'date'
-            },
-            name: 'aDate',
-            id: 'Foo.aDate'
-        },
-        detailOnly: {
-            type: {
-                name: 'string'
-            },
-            name: 'detailOnly',
-            id: 'Foo.detailOnly',
-            detailOnly: true
-        },
-        readOnly: {
-            type: {
-                name: 'string'
-            },
-            name: 'readOnly',
-            id: 'Foo.readOnly',
-            readOnly: true
+        {
+          type: 'minMax',
+          value: {
+            min: 123,
+            max: 456
+          }
+        }
+      ]
+    },
+    withExtra: {
+      type: {
+        name: 'string'
+      },
+      name: 'withExtra',
+      id: 'Foo.withExtra'
+    },
+    withArray: {
+      type: {
+        name: 'array',
+        type: {
+          name: 'object',
+          type: {
+            name: 'Foo'
+          }
+        }
+      },
+      name: 'withArray',
+      id: 'Foo.withArray'
+    },
+    arrayOfPrimitives: {
+      type: {
+        name: 'array',
+        type: {
+          name: 'string'
+        }
+      },
+      name: 'arrayOfPrimitives',
+      id: 'Foo.arrayOfPrimitives'
+    },
+    arrayOfDates: {
+      type: {
+        name: 'array',
+        type: {
+          name: 'date'
+        }
+      },
+      name: 'arrayOfDates',
+      id: 'Foo.arrayOfDates'
+    }
+  },
+  operations: {
+    create: {
+      method: 'post',
+      type: 'create',
+      path: '/api/foo-path'
+    },
+    foo: {
+      method: 'put',
+      type: 'foo',
+      path: '/api/foo-path'
+    },
+    listByPage: {
+      method: 'get',
+      type: 'listByPage',
+      path: '/api/foo-path',
+      params: {
+        bar: {
+          id: 'Foo.filter.bar',
+          name: 'bar',
+          type: {
+            name: 'string'
+          }
         },
         status: {
+          id: 'Foo.filter.status',
+          name: 'status',
+          type: {
+            name: 'enum',
+            values: ['a', 'b', 'c']
+          }
+        },
+        arr: {
+          id: 'Foo.filter.arr',
+          name: 'arr',
+          type: {
+            name: 'array',
             type: {
-                name: 'enum',
-                values: ['a', 'b', 'c']
-            },
-            name: 'status',
-            id: 'Foo.status'
-        },
-        withValidations: {
-            type: {
-                name: 'string'
-            },
-            name: 'withValidations',
-            id: 'Foo.withValidations',
-            validations: [
-                {
-                    type: 'pattern',
-                    value: /^abc.*$/
-                },
-                {
-                    type: 'length',
-                    value: {
-                        min: 2,
-                        max: 5
-                    }
-                },
-                {
-                    type: 'minMax',
-                    value: {
-                        min: 123,
-                        max: 456
-                    }
-                }
-            ]
-        },
-        withExtra: {
-            type: {
-                name: 'string'
-            },
-            name: 'withExtra',
-            id: 'Foo.withExtra'
-        },
-        withArray: {
-            type: {
-                name: 'array',
-                type: {
-                    name: 'object',
-                    type: {
-                        name: 'Foo'
-                    }
-                }
-            },
-            name: 'withArray',
-            id: 'Foo.withArray'
-        },
-        arrayOfPrimitives: {
-            type: {
-                name: 'array',
-                type: {
-                    name: 'string'
-                }
-            },
-            name: 'arrayOfPrimitives',
-            id: 'Foo.arrayOfPrimitives'
-        },
-        arrayOfDates: {
-            type: {
-                name: 'array',
-                type: {
-                    name: 'date'
-                }
-            },
-            name: 'arrayOfDates',
-            id: 'Foo.arrayOfDates'
-        }
-    },
-    operations: {
-        create: {
-            method: 'post',
-            type: 'create',
-            path: '/api/foo-path'
-        },
-        foo: {
-            method: 'put',
-            type: 'foo',
-            path: '/api/foo-path'
-        },
-        listByPage: {
-            method: 'get',
-            type: 'listByPage',
-            path: '/api/foo-path',
-            params: {
-                bar: {
-                    id: 'Foo.filter.bar',
-                    name: 'bar',
-                    type: {
-                        name: 'string'
-                    }
-                },
-                status: {
-                    id: 'Foo.filter.status',
-                    name: 'status',
-                    type: {
-                        name: 'enum',
-                        values: ['a', 'b', 'c']
-                    }
-                },
-                arr: {
-                    id: 'Foo.filter.arr',
-                    name: 'arr',
-                    type: {
-                        name: 'array',
-                        type: {
-                            name: 'string'
-                        }
-                    }
-                },
-                enumArray: {
-                    id: 'Foo.filter.enumArray',
-                    name: 'enumArray',
-                    type: {
-                        name: 'array',
-                        type: {
-                            name: 'enum',
-                            values: ['Value_A', 'Value_B', 'Value_C']
-                        }
-                    }
-                },
-                anInterval: {
-                    id: 'Foo.filter.anInterval',
-                    name: 'anInterval',
-                    type: {
-                        name: 'object',
-                        type: {
-                            name: 'DateInterval'
-                        }
-                    },
-                    required: true
-                }
+              name: 'string'
             }
+          }
+        },
+        enumArray: {
+          id: 'Foo.filter.enumArray',
+          name: 'enumArray',
+          type: {
+            name: 'array',
+            type: {
+              name: 'enum',
+              values: ['Value_A', 'Value_B', 'Value_C']
+            }
+          }
+        },
+        anInterval: {
+          id: 'Foo.filter.anInterval',
+          name: 'anInterval',
+          type: {
+            name: 'object',
+            type: {
+              name: 'DateInterval'
+            }
+          },
+          required: true
         }
-    },
-    checksum: '5430e603f87f2c69b60f488b6157cfd3'
-}
+      }
+    }
+  },
+  checksum: '5430e603f87f2c69b60f488b6157cfd3'
+};
